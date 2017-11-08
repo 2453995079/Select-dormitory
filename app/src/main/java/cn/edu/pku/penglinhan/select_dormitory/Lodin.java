a4462fc0a9b9901db63fdcadd4ed1e4618c4b644
penglinhan <2453995079@qq.com>
2017-11-08 16:23:31 +0800

    登陆界面的完善

---
+++ app/src/main/java/cn/edu/pku/penglinhan/select_dormitory/Lodin.java
@@ -0,0 +1,17 @@
+package cn.edu.pku.penglinhan.select_dormitory;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2017/11/1 0001.
+ */
+
+public class Lodin extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.select_lodin);
+    }
+}
